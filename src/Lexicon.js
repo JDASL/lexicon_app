77134393451595b56de280865644051c42b84a0c
JDASL <127839733+JDASL@users.noreply.github.com>
2025-03-03 17:48:45 +0100

    created Results component, Meaning component

--- src/Lexicon.js
+++ src/Lexicon.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Lexicon() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log();
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
     // documentation: https://dictionaryapi.dev/
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
-
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -28,6 +28,7 @@ export default function Lexicon() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
